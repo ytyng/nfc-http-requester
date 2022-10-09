2e0bbbb2999844bce7ce0b9aa25ef57e4c499544
ytyng <ytyng@live.jp>
2022-10-09 12:15:05 +0900

    update readme

--- README.rst
+++ README.rst
@@ -1,33 +1,44 @@
-git-branch-selector
+nfc-http-requester
 ~~~~~~~~~~~~~~~~~~~
 
-.. image:: https://img.shields.io/pypi/v/git-branch-selector.svg
-    :target: https://pypi.python.org/pypi/git-branch-selector/
+.. image:: https://img.shields.io/pypi/v/nfc-http-requester.svg
+    :target: https://pypi.python.org/pypi/nfc-http-requester/
     :alt: Latest PyPI version
 
-.. image:: ./media/screenshot.png
 
 Install
 -------
 ::
 
-  pip install git-branch-selector
+  python3 -m pip install nfc-http-requester
 
-or
-
-::
-
-  python3 -m pip install git-branch-selector
 
 Usage
 -----
 
+Start daemon:
+
 ::
 
-  git-branch-selector
+  nfc-http-requester --url="https://www.example.com"
 
 
-- arrow-up / down / j / k to move cursor
-- enter to select branch
-- q to quit
-- ctrl-c to quit
+What is it
+----------
+
+デバイスに接続した NFC カードリーダーにカードをタッチした時、
+そのカードの ID と NDEF データを指定した URL に POST 送信します。
+
+社員証を使った出退勤管理システムや、物品貸し出しシステムの
+フロントエンドとしての利用を想定しています。
+
+Support devices
+---------------
+
+nfcpy を使っているので、nfcpy の対応デバイスが使えます。
+
+https://nfcpy.readthedocs.io/en/latest/overview.html#supported-devices
+
+Sony の RC-S380 などが使用できます。 開発は RC-S380 で行っています。
+
+
--- nfc_http_requester/nfc_http_requester.py
+++ nfc_http_requester/nfc_http_requester.py
@@ -1,45 +1,149 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+import argparse
+import datetime
 import time
-
+from typing import Optional, Callable
+from dataclasses import dataclass
+import logging
+import requests
 import nfc
+import uuid
 
-latest_identifier = None
+_logger = logging.getLogger('nfc_http_requester')
 
 
-def connected(tag):
-    global latest_identifier
+@dataclass
+class NfcHttpRequesterState:
+    # 最後にタッチした NFC の ID (7bytes)
+    latest_identifier: bytes = b''
+    latest_processed_datetime: datetime.datetime = datetime.datetime.now()
+    url: str = ''
+    response_handler: Optional[Callable] = None
+
+    def update_latest_processed_datetime(self):
+        self.latest_processed_datetime = datetime.datetime.now()
+
+    def update_latest_processed_datetime_is_old(self):
+        return (datetime.datetime.now() - self.latest_processed_datetime
+                ).total_seconds() > 30
+
+
+_state: Optional[NfcHttpRequesterState] = None
+
+
+def _parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--url', type=str, dest='url', required=True
+    )
+    parser.add_argument(
+        '--frontend-path', type=str, dest='frontend_path',
+        required=False, default='usb',
+        help='ContactlessFrontend path. default is "usb"'
+    )
+    return parser.parse_args()
+
+
+def _get_ndef_records_data(tag) -> list:
+    if not tag.ndef or not tag.ndef.is_readable or not tag.ndef.length:
+        return []
+
+    def _to_dict(record):
+        return {
+            'type': record.type,
+            'name': record.name,
+            'data': record.data.decode('utf-8'),
+        }
+
+    return [
+        _to_dict(record) for record in tag.ndef.records
+    ]
+
+
+def _request(tag, ndef_data: list):
+    transaction_id = uuid.uuid4().hex
+    nfc_id = tag.identifier.hex()
+    _logger.info(f'transaction:{transaction_id}, nfc_id:{nfc_id}')
+    # debug data
+    _logger.debug(('request tag_data:\n' + '\n'.join(
+        [f'  {r}' for r in tag.dump()])))
+    _logger.debug(('request ndef_data:\n' + '\n'.join(
+        [f'  {r}' for r in ndef_data])))
+
+    response = requests.post(
+        _state.url,
+        json={
+            'identifier': nfc_id,
+            'transaction_id': transaction_id,
+            'ndef': ndef_data,
+        }
+    )
+
+    _logger.debug(f'response status:{response.status_code}, '
+                  f'headers:{response.headers}')
+    _logger.debug(f'response content: {response.content.decode()}')
+
+    _state.update_latest_processed_datetime()
+
+    if _state.response_handler:
+        _state.response_handler(response)
+
+
+def _response_handler(response: requests.Response):
+    if not response.ok:
+        _logger.warning(f'_response_handler: response is NG. abort')
+        return
+
+    data = response.json()
+    _logger.info(data)
+
+
+def on_connected(tag):
     # IDm, PMM等を出力
-    print(f'class={tag.__class__.__name__}, str={tag}, id={tag.identifier.hex()}')
-    if tag.identifier == latest_identifier:
+    _logger.debug(
+        f'class={tag.__class__.__name__}, str={tag}, '
+        f'id={tag.identifier.hex()}')
+    if (
+        tag.identifier == _state.latest_identifier and
+        not _state.update_latest_processed_datetime_is_old()
+    ):
+        # 最後にタッチした NFC と同じ ID で、かつ、30秒以内の場合、処理をしない
         return tag
     else:
-        latest_identifier = tag.identifier
-    # 内容を16進数で出力する
-    print(('  ' + '\n  '.join(tag.dump())))
-    if tag.ndef:
-        print("NDEF Capabilities:")
-        print("  readable  = %s" % ("no", "yes")[tag.ndef.is_readable])
-        print("  writeable = %s" % ("no", "yes")[tag.ndef.is_writeable])
-        print("  capacity  = %d byte" % tag.ndef.capacity)
-        print("  message   = %d byte" % tag.ndef.length)
-        if tag.ndef.length > 0:
-            print("NDEF Message:")
-            for i, record in enumerate(tag.ndef.records):
-                print("record", i + 1)
-                print("  type =", repr(record.type))
-                print("  name =", repr(record.name))
-                print("  data =", repr(record.data))
-                # response = requests.get("https://repr(record.data)")
-                # print(curlify.to_curl(response.request))
+        _state.latest_identifier = tag.identifier
+
+    _request(tag, _get_ndef_records_data(tag))
+
     return tag
 
 
-if __name__ == '__main__':
+def setup_logger():
+    _logger.setLevel(logging.DEBUG)
+    _handler = logging.StreamHandler()
+    _handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(thread)d %(name)s %(levelname)s %(message)s'
+    ))
+    _logger.addHandler(_handler)
+
+
+def main():
+    setup_logger()
+    args = _parse_arguments()
+
+    global _state
+    _state = NfcHttpRequesterState(
+        url=args.url,
+        response_handler=_response_handler,
+    )
+
     # タッチ時のハンドラを設定して待機する
-    clf = nfc.ContactlessFrontend('usb')
+    clf = nfc.ContactlessFrontend(args.frontend_path)
     while True:
-        tag = clf.connect(rdwr={'on-connect': connected})
+        tag = clf.connect(rdwr={'on-connect': on_connected})
         if not tag:
             break
         time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
